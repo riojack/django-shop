7bfb751d0005318d8a1358ec5c977758b00a4be8
riojack <ayjay122@gmail.com>
2019-12-10 15:57:15 -0600

    misc: add starting files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-shop-pkg-riojack",
+    version="0.0.1",
+    author="riojack",
+    author_email="riojack@github.com",
+    description="A shop built on Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/riojack/django-shop",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
