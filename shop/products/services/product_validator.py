3134c90756533453bc7358ac430ecaa632384432
riojack <ayjay122@gmail.com>
2019-12-12 15:51:08 -0600

    test: product invalid if no description text

--- shop/products/services/product_validator.py
+++ shop/products/services/product_validator.py
@@ -1,3 +1,5 @@
 class ProductValidator:
     def validate(self, product):
+        if 'description' in product and product['description'] == '':
+            return ["Product does not have text in it's description field"]
         return ["Product does not have a description field"]
--- shop/products/services/tests.py
+++ shop/products/services/tests.py
@@ -4,7 +4,7 @@ from .product_validator import ProductValidator
 
 
 class ProductValidatorTests(TestCase):
-    def test_should_fail_if_product_has_no_description(self):
+    def test_should_fail_if_product_has_no_description_field(self):
         product = {
             'unit_weight': '14.4 oz',
             'count': 250,
@@ -15,3 +15,18 @@ class ProductValidatorTests(TestCase):
         errors = validator.validate(product)
 
         assert "Product does not have a description field" in errors
+
+    def test_should_fail_if_product_has_no_description_text(self):
+        product = {
+            'description': '',
+            'unit_weight': '14.4 oz',
+            'count': 250,
+            'upc': '78087204980',
+            'msrp': '$7.00',
+        }
+
+        validator = ProductValidator()
+        errors = validator.validate(product)
+
+        assert len(errors) == 1
+        assert "Product does not have text in it's description field" in errors
