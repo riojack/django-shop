1bffa34311b4ace0ea9df2e97d7fe4d3724c4738
riojack <ayjay122@gmail.com>
2019-12-12 16:18:03 -0600

    test: product valid if all fields are present

--- shop/products/services/product_validator.py
+++ shop/products/services/product_validator.py
@@ -1,5 +1,9 @@
 class ProductValidator:
     def validate(self, product):
+        if 'description' not in product:
+            return ["Product does not have a description field"]
+
         if 'description' in product and product['description'] == '':
             return ["Product does not have text in it's description field"]
-        return ["Product does not have a description field"]
+
+        return []
--- shop/products/services/tests.py
+++ shop/products/services/tests.py
@@ -4,6 +4,20 @@ from .product_validator import ProductValidator
 
 
 class ProductValidatorTests(TestCase):
+    def test_should_return_empty_list_if_product_has_all_expected_fields(self):
+        product = {
+            'description': '11 oz Beverage Mug',
+            'unit_weight': '14.4 oz',
+            'count': 250,
+            'upc': '78087204980',
+            'msrp': '$7.00',
+        }
+
+        validator = ProductValidator()
+        errors = validator.validate(product)
+
+        self.assertEqual(len(errors), 0)
+
     def test_should_fail_if_product_has_no_description_field(self):
         product = {
             'unit_weight': '14.4 oz',
